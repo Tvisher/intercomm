ad2cd8efea4dd8c8ae8ccf399658a6c46ae04b1e
Tvisher <tvisher@mail.ru>
2023-02-16 15:18:53 +0600

    css fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 import * as baseFunction from './modules/functions.js';
-// import AOS from 'aos';
+import AOS from 'aos';
 import IMask from 'imask';
 
 // Проверка поддержки webP
@@ -8,25 +8,25 @@ baseFunction.testWebP();
 
 
 // // Инит и опции библиотеки анимаций
-// AOS.init({
-//     // Global settings:
-//     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-//     startEvent: 'load', // name of the event dispatched on the document, that AOS should initialize on
-//     initClassName: 'aos-init', // class applied after initialization
-//     animatedClassName: 'aos-animate', // class applied on animation
-//     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-//     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-//     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-//     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-//     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-//     offset: 25, // offset (in px) from the original trigger point
-//     delay: 100, // values from 0 to 3000, with step 50ms
-//     duration: 1200, // values from 0 to 3000, with step 50ms
-//     easing: 'ease', // default easing for AOS animations
-//     once: false, // whether animation should happen only once - while scrolling down
-//     mirror: false, // whether elements should animate out while scrolling past them
-//     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-// });
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'load', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 25, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 1200, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
 
 //логика работы меню бургер
 document.body.addEventListener('click', (e) => {
@@ -86,78 +86,74 @@ const mainSliderComponent = new Swiper('.main-slider__component', {
 })();
 
 
-(function () {
-    const scrollTrigger = document.querySelector('.main-screen__scroll-trigger');
-    const flyingHeader = document.querySelector('.fixed-header');
-    const dataReverseTriger = document.querySelector('[data-reverse-triger]');
 
-    var scrollPos = 0;
-    let sctollDirection;
-    $(window).scroll(function () {
-        var st = $(this).scrollTop();
-        if (st > scrollPos) {
-            sctollDirection = 1;
-        } else {
-            sctollDirection = -1;
+
+function lockScroll(elem) {
+    if (elem.addEventListener) {
+        elem.addEventListener("DOMMouseScroll", elem.onmousewheel = function (e) {
+            e.preventDefault();
+        }, false);
+    } else {
+        elem.onmousewheel = function (e) {
+            window.event.returnValue = false;
         }
-        scrollPos = st;
-    });
+    }
+}
+
+function unlockScroll(elem) {
+    if (elem.removeEventListener) {
+        elem.removeEventListener("DOMMouseScroll", elem.onmousewheel, false);
+        elem.onmousewheel = null;
+    } else {
+        elem.onmousewheel = null;
+    }
+}
 
 
-    function lockScroll(elem) {
-        if (elem.addEventListener) {
-            elem.addEventListener("DOMMouseScroll", elem.onmousewheel = function (e) {
-                e.preventDefault();
-            }, false);
-        } else {
-            elem.onmousewheel = function (e) {
-                window.event.returnValue = false;
-            }
+const scrollTrigger = document.querySelector('.main-screen__scroll-trigger');
+const flyingHeader = document.querySelector('.fixed-header');
+const dataReverseTriger = document.querySelector('[data-reverse-triger]');
+
+var scrollPos = 0;
+let sctollDirection = 1;
+$(window).scroll(function () {
+    var st = $(this).scrollTop();
+    if (st > scrollPos) {
+        sctollDirection = 1;
+    } else {
+        sctollDirection = -1;
+    }
+    scrollPos = st;
+});
+
+if (scrollTrigger) {
+    let trigerCounter = 0
+
+    function scrollToMedia() {
+        if (window.innerWidth < 1360) return;
+        let dataTrigerValue = dataReverseTriger.getBoundingClientRect().top - flyingHeader.clientHeight;
+        console.log(window.scrollY < 10, trigerCounter == 0, dataTrigerValue > 0, sctollDirection > 0);
+        if (window.scrollY < 10 && trigerCounter == 0 && dataTrigerValue > 0 && sctollDirection > 0) {
+            trigerCounter = 1;
+            document.body.classList.add('no-click');
+            lockScroll(document.querySelector('body'));
+            $('html, body').animate({
+                scrollTop: dataTrigerValue,
+            }, 600, 'swing', function () {
+                trigerCounter = 0;
+                unlockScroll(document.querySelector('body'));
+                document.body.classList.remove('no-click');
+            });
         }
     }
 
-    function unlockScroll(elem) {
-        if (elem.removeEventListener) {
-            elem.removeEventListener("DOMMouseScroll", elem.onmousewheel, false);
-            elem.onmousewheel = null;
-        } else {
-            elem.onmousewheel = null;
-        }
-    }
+    window.addEventListener("scroll", scrollToMedia);
+    document.querySelector('.scroll-helper').addEventListener('click', () => {
+        sctollDirection = 1;
+        scrollToMedia()
+    })
 
-
-    if (scrollTrigger) {
-        let trigerCounter = 0
-        window.addEventListener("scroll", (e) => {
-            if (window.innerWidth < 1360) return;
-
-            let dataTrigerValue = dataReverseTriger.getBoundingClientRect().top - flyingHeader.clientHeight;
-            if (window.scrollY < 10 && trigerCounter == 0 && dataTrigerValue > 0 && sctollDirection > 0) {
-                console.log('Прокрутка к Медиа');
-                trigerCounter = 1;
-                document.body.classList.add('no-click');
-                lockScroll(document.querySelector('body'));
-                $('html, body').animate({
-                    scrollTop: dataTrigerValue,
-                }, 1800, 'swing', function () {
-                    trigerCounter = 0;
-                    console.log("Animation complete");
-                    unlockScroll(document.querySelector('body'));
-                    document.body.classList.remove('no-click');
-                });
-            }
-
-            // if (sctollDirection < 0 && dataTrigerValue > 0) {
-            //     console.log('Прокрутка к началу');
-            //     $('html, body').animate({
-            //         scrollTop: 0
-            //     }, 2000, function () {
-            //         console.log("Animation complete");
-            //     });
-            // }
-        })
-    }
-})();
+}
 
 
 function validateEmail(email) {
